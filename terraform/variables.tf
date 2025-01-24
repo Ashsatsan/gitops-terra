096d1e4eca6ba35b6e97aadd68c0b5622b9bfc3e
ahundal6 <134111888+Ashsatsan@users.noreply.github.com>
2025-01-23 23:14:06 -0500

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,3 +9,26 @@ variable "clusterName" {
   type        = string
   default     = "vprofile1-eks"
 }
+
+variable "github_org" {
+  description = "GitHub organization name"
+  type        = string
+  default     = "Ashsatsan"
+
+  validation {
+    condition     = var.github_org == "Ashsatsan"
+    error_message = "The github_org must be 'Ashsatsan'."
+  }
+}
+
+variable "github_repo" {
+  description = "GitHub repository name"
+  type        = string
+  default     = "gitops-argocd"
+
+  validation {
+    condition     = var.github_repo == "gitops-argocd"
+    error_message = "The github_repo must be 'gitops-argocd'."
+  }
+}
+
